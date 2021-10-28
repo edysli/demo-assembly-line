61cc5615afba67de78a8fd4ce9c90813c7661494
Etienne Dysli Metref <etienne.dysli@gmail.com>
2021-10-28 21:08:50 +0200

    Implement method AssemblyLine.produce() and checks
    Implement checks on the list of stations inside `AssemblyLine`'s init.
    Implement method `AssemblyLine.produce()`, still without returning a
    `Car`.

--- src/main/kotlin/demo/assemblyline/AssemblyLine.kt
+++ src/main/kotlin/demo/assemblyline/AssemblyLine.kt
@@ -1,6 +1,12 @@
 package demo.assemblyline
 
-class AssemblyLine(val stations: List<Station>) {
+class AssemblyLine(private val stations: List<Station>) {
+  init {
+    if (stations.size < 2) throw IllegalStateException("At least two stations are required")
+    if (stations.last() !is QualityAssuranceAndBuildStation) throw IllegalStateException("The last station must be a quality assurance and build station")
+  }
+
   fun produce(assemblyCar: AssemblyCar) {
+    for (s in stations) s.execute(assemblyCar)
   }
 }
--- src/test/kotlin/demo/assemblyline/AssemblyLineTest.kt
+++ src/test/kotlin/demo/assemblyline/AssemblyLineTest.kt
@@ -1,18 +1,37 @@
 package demo.assemblyline
 
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class AssemblyLineTest {
   @Test
-  fun takesAListOfStations() {
-    val stations: List<Station> = listOf()
-    val al: AssemblyLine = AssemblyLine(stations)
+  fun needsAtLeastTwoStations() {
+    val ise = assertThrows<IllegalStateException> { AssemblyLine(listOf(PaintingStation())) }
+    assertEquals("At least two stations are required", ise.message)
   }
 
   @Test
-  fun hasProduceMethod() {
-    val stations: List<Station> = listOf()
-    val al: AssemblyLine = AssemblyLine(stations)
-    al.produce(AssemblyCarEntity())
+  fun lastStationMustBeQualityAssuranceAndBuild() {
+    val ise = assertThrows<IllegalStateException> { AssemblyLine(listOf(PaintingStation(), MechanicAssemblyStation())) }
+    assertEquals("The last station must be a quality assurance and build station", ise.message)
+  }
+
+  @Test
+  fun produceCallsAllStations() {
+    val al: AssemblyLine = AssemblyLine(
+      listOf(
+        PaintingStation(),
+        MechanicAssemblyStation(),
+        InteriorAssemblyStation(),
+        QualityAssuranceAndBuildStation()
+      )
+    )
+    val assemblyCar = AssemblyCarSpy()
+    al.produce(assemblyCar)
+    assertEquals(1, assemblyCar.paintCalls)
+    assertEquals(1, assemblyCar.assemblyMechanicsCalls)
+    assertEquals(1, assemblyCar.assemblyInteriorCalls)
+    assertEquals(1, assemblyCar.buildCalls)
   }
 }
